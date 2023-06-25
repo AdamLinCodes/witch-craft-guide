ca4e5a8eb857dca28c9d2831ec5526e6a4c65966
ADogTheDestroyer <andrewhlin0@gmail.com>
2023-06-25 16:10:35 -0400

    christianity template setup

--- app/christianity/page.tsx
+++ app/christianity/page.tsx
@@ -1,10 +1,77 @@
 
 export default function Christianity() {
   return (
-    <div className="min-h-screen w-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+    <div className="min-h-screen w-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-4">
       <h1 className="font-garamond text-8xl">
-        Christianity Page
+        History of Christianity
       </h1>
+
+      <div className="border-2 border-black bg-white rounded-lg p-8 w-4/5">
+        <h2 className="text-2xl">Roman Catholocism</h2>
+        <p>DESCRIPTION</p>
+        <ul className="list-disc p-4">
+          <li>KEY POINTS</li>
+          <li>Roman Catholicism is the largest Christian denomination worldwide.</li>
+          <li>It traces its origins back to the early Christian Church in Rome.</li>
+          <li>The Roman Catholic Church views itself as the continuation of the original apostolic tradition.</li>
+          <li>The Pope, based in Vatican City, is considered the spiritual leader and head of the Roman Catholic Church.</li>
+          <li>Roman Catholicism places emphasis on the authority of the Pope, who is believed to have apostolic succession from Saint Peter.</li>
+          <li>The Roman Catholic Church has a hierarchical structure with bishops, priests, and deacons.</li>
+          <li>Sacraments play a vital role in Roman Catholicism, including Baptism, Holy Eucharist, Confirmation, Penance (Confession), Anointing of the Sick, Holy Orders, and Matrimony.</li>
+          <li>The Mass, or Eucharistic celebration, is a central part of Roman Catholic worship.</li>
+          <li>The veneration of saints, including Mary as the Mother of God, is an important aspect of Roman Catholic devotion.</li>
+          <li>Roman Catholicism believes in the doctrine of transubstantiation, teaching that the bread and wine in the Eucharist become the actual body and blood of Jesus Christ.</li>
+        </ul>
+      </div><br/><br/>
+
+      <div className="border-2 border-black bg-white rounded-lg p-8 w-4/5">
+        <h2 className="text-2xl">Protestantism</h2>
+        <ul className="list-disc p-4">
+          <li>KEY POINTS</li>
+          <li>Roman Catholicism is the largest Christian denomination worldwide.</li>
+          <li>It traces its origins back to the early Christian Church in Rome.</li>
+          <li>The Roman Catholic Church views itself as the continuation of the original apostolic tradition.</li>
+          <li>The Pope, based in Vatican City, is considered the spiritual leader and head of the Roman Catholic Church.</li>
+          <li>Roman Catholicism places emphasis on the authority of the Pope, who is believed to have apostolic succession from Saint Peter.</li>
+          <li>The Roman Catholic Church has a hierarchical structure with bishops, priests, and deacons.</li>
+          <li>Sacraments play a vital role in Roman Catholicism, including Baptism, Holy Eucharist, Confirmation, Penance (Confession), Anointing of the Sick, Holy Orders, and Matrimony.</li>
+          <li>The Mass, or Eucharistic celebration, is a central part of Roman Catholic worship.</li>
+          <li>The veneration of saints, including Mary as the Mother of God, is an important aspect of Roman Catholic devotion.</li>
+          <li>Roman Catholicism believes in the doctrine of transubstantiation, teaching that the bread and wine in the Eucharist become the actual body and blood of Jesus Christ.</li>
+        </ul>
+      </div><br/><br/>
+
+      <div className="border-2 border-black bg-white rounded-lg p-8 w-4/5">
+        <h2 className="text-2xl">Anglicanism</h2>
+        <ul className="list-disc p-4">
+          <li>Roman Catholicism is the largest Christian denomination worldwide.</li>
+          <li>It traces its origins back to the early Christian Church in Rome.</li>
+          <li>The Roman Catholic Church views itself as the continuation of the original apostolic tradition.</li>
+          <li>The Pope, based in Vatican City, is considered the spiritual leader and head of the Roman Catholic Church.</li>
+          <li>Roman Catholicism places emphasis on the authority of the Pope, who is believed to have apostolic succession from Saint Peter.</li>
+          <li>The Roman Catholic Church has a hierarchical structure with bishops, priests, and deacons.</li>
+          <li>Sacraments play a vital role in Roman Catholicism, including Baptism, Holy Eucharist, Confirmation, Penance (Confession), Anointing of the Sick, Holy Orders, and Matrimony.</li>
+          <li>The Mass, or Eucharistic celebration, is a central part of Roman Catholic worship.</li>
+          <li>The veneration of saints, including Mary as the Mother of God, is an important aspect of Roman Catholic devotion.</li>
+          <li>Roman Catholicism believes in the doctrine of transubstantiation, teaching that the bread and wine in the Eucharist become the actual body and blood of Jesus Christ.</li>
+        </ul>
+      </div><br/><br/>
+
+      <div className="border-2 border-black bg-white rounded-lg p-8 w-4/5">
+        <h2 className="text-2xl">Calvinism</h2>
+        <ul className="list-disc p-4">
+          <li>Roman Catholicism is the largest Christian denomination worldwide.</li>
+          <li>It traces its origins back to the early Christian Church in Rome.</li>
+          <li>The Roman Catholic Church views itself as the continuation of the original apostolic tradition.</li>
+          <li>The Pope, based in Vatican City, is considered the spiritual leader and head of the Roman Catholic Church.</li>
+          <li>Roman Catholicism places emphasis on the authority of the Pope, who is believed to have apostolic succession from Saint Peter.</li>
+          <li>The Roman Catholic Church has a hierarchical structure with bishops, priests, and deacons.</li>
+          <li>Sacraments play a vital role in Roman Catholicism, including Baptism, Holy Eucharist, Confirmation, Penance (Confession), Anointing of the Sick, Holy Orders, and Matrimony.</li>
+          <li>The Mass, or Eucharistic celebration, is a central part of Roman Catholic worship.</li>
+          <li>The veneration of saints, including Mary as the Mother of God, is an important aspect of Roman Catholic devotion.</li>
+          <li>Roman Catholicism believes in the doctrine of transubstantiation, teaching that the bread and wine in the Eucharist become the actual body and blood of Jesus Christ.</li>
+        </ul>
+      </div>
     </div>
   )
 }
